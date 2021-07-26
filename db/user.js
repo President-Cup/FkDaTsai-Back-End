9b71b8b62a35d79ed8d55c294dd273a2efa59c39
TrevorWu <wowpapa3232@gmail.com>
2021-07-26 23:30:35 +0800

    Sign up feature (#1)
    add dependencies - bcrypt
    Sign up API (/api/v1/signup)

---
+++ db/user.js
@@ -0,0 +1,56 @@
+const { pool } = require("./db");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 15;
+
+async function createUser(userDetails) {
+  const client = await pool.connect();
+
+  let hashedPassword;
+  let result;
+
+  try {
+    await client.query("BEGIN");
+
+    await bcrypt.hash(userDetails.userPassword, saltRounds).then((hash) => {
+      hashedPassword = hash;
+    });
+
+    const insertUserText =
+      "INSERT INTO users(user_name, user_mail, user_password) VALUES ($1, $2, $3);";
+    const insertUserParams = [
+      userDetails.userName,
+      userDetails.userMail,
+      hashedPassword,
+    ];
+
+    result = await client.query(insertUserText, insertUserParams);
+
+    await client.query("COMMIT");
+  } catch (err) {
+    await client.query("ROLLBACK");
+    throw err;
+  } finally {
+    client.release();
+    return result.rowCount == 1;
+  }
+}
+
+async function isUserExists(userMail) {
+  const client = await pool.connect();
+  const findUserText = "SELECT * FROM users WHERE user_mail = $1";
+
+  let isUserExists;
+
+  await client.query(findUserText, [userMail]).then((result) => {
+    client.release();
+    isUserExists = result.rowCount > 0;
+  });
+
+  return isUserExists;
+}
+
+module.exports = {
+  createUser,
+  isUserExists,
+};
--- router/signup/index.js
+++ router/signup/index.js
@@ -1,7 +1,36 @@
 const router = require("express").Router();
+const userDb = require("../../db/user");
 
+/**
+ * Create a new account.
+ * @param {object} req - JSON object that contains the user's information.
+ * @returns {}
+ * TODO: Add a check to make sure the email is valid, use validatorjs.
+ * TODO: Add a check to make sure the password is between 8 and 20 chars long.
+ * TODO: Sanitize input.
+ */
 router.post("/", async (req, res) => {
-  res.status(200).json({ success: false, msg: "This is /api/v1/signup" });
+  const request = req.body;
+
+  const isUserExists = await userDb.isUserExists(request.userMail);
+  if (isUserExists) {
+    // User exists, abort operation.
+    return res
+      .status(400)
+      .json({ success: false, message: "User already exists." });
+  }
+
+  const result = await userDb.createUser(request);
+
+  if (result) {
+    return res
+      .status(201)
+      .json({ success: true, message: "User created successfully." });
+  } else {
+    return res
+      .status(500)
+      .json({ success: false, message: "Error creating user." });
+  }
 });
 
 module.exports = router;
