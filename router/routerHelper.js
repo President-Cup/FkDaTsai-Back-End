fc1c0bf88d804c2ef524956ff3a8d6f1c30c80a7
TrevorWu <wowpapa3232@gmail.com>
2021-07-28 18:40:07 +0800

    Change role to user_role because of the database column naming

--- router/routerHelper.js
+++ router/routerHelper.js
@@ -1,6 +1,6 @@
 const allowOnly = function (accessLevel, callback) {
   function checkUserRole(req, res) {
-    const role = req.user[0].role;
+    const role = req.user[0].user_role;
     
     console.log("Access Level: " + accessLevel + ", User Role: " + role);
 
