8b055e31613292615e9199998e8e0d2c3c5057b9
TrevorWu <wowpapa3232@gmail.com>
2021-07-29 21:32:20 +0800

    Move create item from router to item.js

--- router/item/index.js
+++ router/item/index.js
@@ -1,26 +1,24 @@
 const router = require("express").Router();
 const { pool } = require("../../db/db");
+const itemDb = require("../../db/item");
 
-//Post new item
+// TODO: Erroe handling
+
+/**
+ * Post new item
+ */
 router.post("/", async (req, res) => {
-    const client = await pool.connect();
-    try {
-        const { categoryId } = req.body;
-        const { categorySubId } = req.body;
-        const { itemName } = req.body;
-        const { description } = req.body;
-        const { imageUrl } = req.body;
-        const { price } = req.body;
-        const { cityId } = req.body;
-        const { districtId } = req.body;
-        const { userId } = req.body;
+    const request = req.body;
+    const isCreateSuccess = await itemDb.createNewItem(request);
 
-        const createCategory = await client.query('INSERT INTO item (user_id, category_id, category_sub_id, city_id, district_id, item_name, description, image_url, price) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)',
-            [userId, categoryId, categorySubId, cityId, districtId, itemName, description, imageUrl, price]);
-        //res.json(createCategory.rows[0]);
-        res.json(createCategory);
-    } catch (err) {
-        console.error(err.message)
+    if (isCreateSuccess) {
+      return res
+        .status(201)
+        .json({ success: true, message: "Item created." });
+    } else {
+        return res
+          .status(500)
+          .json({ success: false, message: "We fucked up." });
     }
 });
 
